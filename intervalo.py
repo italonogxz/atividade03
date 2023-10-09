a42221c1d0198474d93c09c49901b80436217e8c
italonogxz <italo.nogueira6@aluno.ce.gov.br>
2023-10-09 15:36:12 -0300

    atividade 03

---
+++ intervalo.py
@@ -0,0 +1,9 @@
+import time
+
+print('mamão')
+time.sleep(5)
+print('manga')
+time.sleep(5)
+print('laranja')
+                                                                                                                                        
+
